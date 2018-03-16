1ca8f5266c980fef4ac2b5e5ea7e0e2dbad7a874
IDAlearning <idazou85@gmail.com>
2018-03-16 15:27:06 +0800

    parallax_js

--- parallax_Test/js/main.js
+++ parallax_Test/js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-	
-	/* Scroll event handler */
+
     $(window).bind('scroll',function(e){
     	parallaxScroll();
-		// redrawDotNav();
+
     });
 });
 
-/* Scroll the background layers */
+
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
 	$('#bg1').css('top',(0-(scrolled*.25))+'px');
